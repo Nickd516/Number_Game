4ce9d2b5acaab7c25a27c14398dce8f8b2cd0886
Nickd516 <122472557+Nickd516@users.noreply.github.com>
2023-01-11 11:04:14 -0600

    Add files via upload

---
+++ number_guess_final.js
@@ -0,0 +1,52 @@
+
+let valid_input = false;
+let max_number, number;
+let max = document.getElementById("guessline");
+
+while(!valid_input) {
+    maxnum = prompt("What should the max number be?");
+    max_number = Number(maxnum);
+
+    if (!isNaN(maxnum) && maxnum > 1) {
+        valid_input = true;
+    }
+realnum = (Math.round(maxnum))
+ranNum = (Math.floor(Math.random() * realnum)) + 1;
+console.log(ranNum) //see the number you are trying to guess
+max.innerHTML = "Guess a number between 1 and " + realnum;
+
+}
+
+let numArray = []
+
+function do_guess() {
+    let guess = Number(document.getElementById("guess").value);
+    let message = document.getElementById("message");
+
+    if (guess == ranNum) {
+        if (numArray.includes(guess) === false)numArray.push(guess);
+        message.innerHTML = "You got it!! It took you " + numArray.length + " tries! <br> Your guesses were: " + numArray;
+    }
+    else if(guess > ranNum) {
+        message.innerHTML = "Your number is too high. Try a Lower Number!";
+        if (numArray.includes(guess) === false)numArray.push(guess)
+        else message.innerHTML = "You have already guessed this number!"
+    }
+    else if(isNaN(guess)) {
+        message.innerHTML = "Your guess is not a number. Try again"
+    } 
+    else if(guess<1) {
+        message.innerHTML = "The number can't be less than 1";
+    }
+    else if(guess < ranNum) {
+        message.innerHTML = "Your number is too low. Try a Higher Number!";
+        if(numArray.includes(guess) === false)numArray.push(guess)
+        else message.innerHTML = "You have already guessed this number!"
+    }  
+    else{
+        message.innerHTML = "That number is not in range, Try Again.";
+    }
+}
+function reloadpage(){
+    window.location.reload();
+}
